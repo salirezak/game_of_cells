4221bccb60666116baf9a300659e7c1d167fe54a
AlirezaKarami <salirezakarami@gmail.com>
2021-09-12 07:38:07 +0430

    add division func

--- game_of_cells.py
+++ game_of_cells.py
@@ -1,4 +1,5 @@
 import random
+from copy import deepcopy as cp
 
 class Room:
         global world, height, width
@@ -7,6 +8,13 @@ class Room:
                 self.x = x
                 self.y = y
                 self.cell = " "
+
+                self.neighbours_location = []
+                for i in range(-1,2):
+                        for j in range(-1,2):
+                                if x+i in range(0, height) and y+j in range(0, width):
+                                        self.neighbours_location.append((x+i, y+j))
+                self.neighbours_location.remove((x,y)) 
 
         def empties(self):
                 empties = []
@@ -38,6 +46,12 @@ def insert_cells(cells):
                 for x,y in cells[cell_type]:
                        world[x][y].cell = cell_type  
 
+def divison(x,y):
+        global world, height, width
+
+        empties = world[x][y].empties()
+        if empties:
+                return random.choice(empties)  
 
 height, width = 5, 5
 
@@ -45,7 +59,28 @@ height, width = 5, 5
 create_world()
 insert_cells({
         "A" : [(0,0), (0,1), (1,2), (2,2)],
-        #"B" : [(4,4)]
+        #"B" : [(4,4)]seyed1378alireza
         })
 
 show_world()
+
+                                       
+while True:
+        tmp = cp(world)
+        for x in range(height):
+                for y in range(width):
+                        if world[x][y].cell != " ":
+                                ij = divison(x, y)
+                                if ij:
+                                        i, j = ij
+                                        tmp[i][j].cell = world[x][y].cell
+        world = cp(tmp)
+        del tmp
+
+        print("")
+        
+        show_world()
+
+
+
+                                               
